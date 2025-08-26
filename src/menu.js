eef2355cb4972407ec9ab75c1403a718d0d68b08
ArquitectoIA <arquitectoia@agenciaredzilla.com>
2025-08-26 17:15:53 -0500

    Implementacion de funciones

---
+++ src/menu.js
@@ -0,0 +1,101 @@
+export const menu = {
+  //Hamburguesas
+  "Hamburguesa Tracional": 21500,
+  "Hamburguesa Chicken": 21500,
+  "Hamburguesa Especial": 27500,
+  "Hamburguesa Mixta": 27500,
+  "Hamburguesa Doble carne": 27500,
+  "Hamburguesa Cheese": 27500,
+  "Hamburguesa Ranchera": 28500,
+  "Hamburguesa Madureña": 30500,
+  "Hamburguesa Cheddar pig": 31000,
+  "Hamburguesa Super flints": 32000,
+  "Hamburguesa Buchona": 32000,
+  "Hamburguesa Rompepanzas": 33000,
+  "Hamburguesa Mega flints": 35000,
+
+  //Perros
+  "Perro Tradicional": 14000,
+  "Perro Tradicional 30cm": 19000,
+  "Perro Pollo": 17500,
+  "Perro Pollo 30cm": 21500,
+  "Perro Carne": 17500,
+  "Perro Carne 30cm": 21500,
+  "Perro Mixto": 18500,
+  "Perro Mixto 30cm": 25000,
+  "Perro Especial": 18000,
+  "Perro Especial 30cm": 24000,
+  "Choriperro Especial": 18000,
+  "Choriperro Especial 30cm": 24500,
+  "Perro Americano": 20500,
+  "Perro Americano 30cm": 27500,
+  "Perro Flints loco": 20500,
+  "Perro Flints loco 30cm": 27500,
+  "Perro Super flints": 22500,
+  "Perro Super flints 30cm": 29500,
+
+  //Patacones
+  "Patacón Pollo": 20000,
+  "Patacón Carne": 20000,
+  "Patacón Mixto": 21000,
+  "Patacón Ranchero": 22500,
+  "Patacón Super flints": 24500,
+
+  //Desgranados
+  "Desgranado Pollo": 23000,
+  "Desgranado Carne": 23000,
+  "Desgranado Mixto": 26000,
+  "Desgranado Super flints": 28500,
+
+  //Salchipapas y choripapas
+  Salchipapa: 18500,
+  Choripapa: 20000,
+  Choripollo: 25000,
+  Choricarne: 25000,
+  "Choriloco super flints mediano": 30000,
+  "Choriloco super flints grande": 38000,
+  "Papas locas pequeñas": 31000,
+  "Papas locas medianas": 39000,
+  "Papas locas grandes": 50000,
+
+  //Picadas
+  "Picada pequeña": 35500,
+  "Picada mediana": 50000,
+  "Picada grande": 60000,
+  "Pechuga a la plancha": 19500,
+  "Carne a la plancha": 20500,
+
+  //Patacones burguer
+  "Patacón burger": 21500,
+  "Patacón burger de pollo": 21500,
+  "Patacón doble carne": 24000,
+  "Patacón burger super flints": 27500,
+};
+
+export const adicionales = {
+  "Porción de Papa Mediana": 7500,
+  "Porción de Papa Grande": 11500,
+  Tocineta: 3500,
+  "Tajada de Queso": 2000,
+  Chorizo: 4500,
+  "Huevo de Codorniz": 2500,
+  "Aros de Cebolla": 3000,
+  "Queso Costeño": 3000,
+  "Salchicha Zenú": 3000,
+  "Salchicha Americana": 4500,
+  Maíz: 4000,
+};
+
+export const bebidas = {
+  "Granizada de limon": 8000,
+  "Granizada de naranja": 8000,
+  "Granizada de mandarina": 8000,
+  "Granizada de maracuya": 8000,
+  "Granizada de yerbabuena": 8000,
+  "Jugo de mora": 10000,
+  "Jugo de fresa": 10000,
+  "Jugo de guanabana": 10000,
+  "Jugo de uva": 10000,
+  Cerezada: 10000,
+  Maracumango: 10000,
+};
